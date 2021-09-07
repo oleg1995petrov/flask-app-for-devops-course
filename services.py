1e393c70642cef29130cd68b8a170957510005b3
Aleh Ventskovich <70842757+oleg1995petrov@users.noreply.github.com>
2021-09-07 22:38:33 +0300

    Update services.py

--- services.py
+++ services.py
@@ -6,35 +6,32 @@ from bs4 import BeautifulSoup as bs
 def get_animal_img(animal):
     url = f'https://emojipedia.org/{animal}'
     response = requests.get(url)
-    if response.status_code != 200:
-        return
+    
+    if response.status_code != 200: return
+    
     soup = bs(response.content, 'html.parser')
     try:
-        res1 = soup.find('div', class_='content')
-        soup = bs(str(res1), 'html.parser')
+        content = soup.find('div', class_='content')
+        soup = bs(str(content), 'html.parser')
         img = soup.find('span').text
-    except AttributeError :
+    except AttributeError:
         return 
     return img
 
 
 def generate_message(animal=None, sound=None, count=None):
+    message = ("You haven't passed all the arguments!\n"
+               "Required arguments are:\n" 
+               "-animal\n"
+               "-sound\n"
+               "-count\n")
+    
     if all((animal, sound, count)):
         img = get_animal_img(animal)
-        if img:
-            message = f'{animal} {img}  says {sound}\n'
-        else:
-            message = f'{animal} says {sound}\n'
-            
-        if isinstance(count, int):
+        message = f'{animal} {img}  says {sound}\n' if img else message = f'{animal} says {sound}\n'
+        
+        if isinstance(count, int): 
             message *= count
+            
         message += 'Made with ❤️  by Ventz.\n'
-    else: 
-        message = (
-            "You haven't passed all the arguments!\n"
-            "Required arguments are:\n" 
-            "-animal\n"
-            "-sound\n"
-            "-count\n"
-        )
     return message 
